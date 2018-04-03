b4049c01ddf71410b90e6ad3e84779bc7aac5dfb
SFoskitt <stephanie.foskitt@gmail.com>
2018-04-02 23:12:29 -0500

    Assign6p2 - initial definition and first pass

--- Assign06P2SuppliedFiles/btNode.cpp
+++ Assign06P2SuppliedFiles/btNode.cpp
@@ -30,28 +30,45 @@ int bst_size(btNode* bst_root)
    return 1 + bst_size(bst_root->left) + bst_size(bst_root->right);
 }
 
-bool bst_insert(btNode* bst_root, btNode* bst_node)
+bool bst_insert(btNode* bst_root, int insVal)
 {
   while(bst_root->left && bst_root->right)
   {
     //if(bst_node-> data > bst_root->data)
-      // if(bst_root->right != 0)
-        // go to the right
-        // bst_root = bst_root->right;
-        // else { // bst_root->right = bst_node}
-        //return true;
+      if(insVal > bst_root->data)
+      {
+         if(bst_root->right != 0)
+         {
+            // go to the right
+            bst_root = bst_root->right;
+         }
+         else
+         {
+            btNode* tmp = 0;
+            tmp->data = insVal;
+            bst_root->right = tmp;
+            return true;
+         }
+      }
 
-    //if(bst_node-> data < bst_root->data)
-      // if(bst_root->left !=0)
-      // go to the left
-      // bst_root = bst_root->left
-        // else { // bst_root->left = bst_node}
-        //return true;
+      if(insVal < bst_root->data)
+      {
+         if(bst_root->left != 0)
+         {
+            bst_root = bst_root->left;
+         }
+         else
+         {
+            btNode* tmp = 0;
+            tmp->data = insVal;
+            bst_root->left = tmp;
+            return true;
+         }
+      }
   }
-  return false;  // node could not be added to tree (maybe a duplicate?)
+  return false;  // node could not be added to tree (maybe a duplicate value?)
 }
 
-// write definition for bst_remove here
 bool bst_remove(btNode* bst_root, int anInt)
 {
   // FIRST FIND THE THING! Use the while loop above to find the correct node to remove
@@ -59,10 +76,9 @@ bool bst_remove(btNode* bst_root, int anInt)
     //
   // case 2
     //
-  return false; // couldn't be done for some reason?
+  return false; // thing is not found
 }
 
-// write definition for bst_remove_max here
 bool bst_remove_max(btNode* bst_root)
 {
   return true;
--- Assign06P2SuppliedFiles/btNode.h
+++ Assign06P2SuppliedFiles/btNode.h
@@ -37,7 +37,7 @@ int bst_size(btNode* bst_root);
 //       If a node with data equals insInt is found, the node's data field
 //       has been overwritten with insInt; no new node has been created.
 // write prototype for bst_insert here
-bool bst_insert(btNode* bst_root, btNode* bst_node);
+bool bst_insert(btNode* bst_root, int insInt);
 
 // pre:  bst_root is root pointer of a binary search tree (may be 0 for
 //       empty tree)
@@ -46,7 +46,7 @@ bool bst_insert(btNode* bst_root, btNode* bst_node);
 //       and the function returns true. Otherwise, if remInt was not in the
 //       tree, then the tree is unchanged, and the function returns false.
 // write prototype for bst_remove here
-bool bst_remove(btNode* bst_root, int anInt);
+bool bst_remove(btNode* bst_root, int remInt);
 
 // pre:  bst_root is root pointer of a non-empty binary search tree
 // post: The largest item in the binary search tree has been removed, and
